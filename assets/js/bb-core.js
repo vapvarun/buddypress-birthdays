eeae467208eb474d4f2f42cbcf55ddd042bcb841
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2021-02-11 17:41:44 +0530

    PHPCS fixes (assets)

--- assets/js/bb-core.js
+++ assets/js/bb-core.js
@@ -4,21 +4,25 @@
 /*
 Jquery Ready!
 */
-jQuery(document).ready(function($){
-    "use strict";
-    /*
-    Add basic front-end page scripts here
-    */
-    /*
-    *   Simple jQuery Click
-    *
-    *   Add id="mySpecialButton" to any element and when 
-    *   clicked the same element will get the class "active".
-    *
-    */
-    $('#mySpecialButton').click(function(){
-        $(this).addClass('active');    
-    });
-    
-    // End basic front-end scripts here
-});
+jQuery( document ).ready(
+	function($){
+		"use strict";
+		/*
+		*   Add basic front-end page scripts here
+		*/
+		/*
+		*   Simple jQuery Click
+		*
+		*   Add id="mySpecialButton" to any element and when
+		*   clicked the same element will get the class "active".
+		*
+		*/
+		$( '#mySpecialButton' ).click(
+			function(){
+				$( this ).addClass( 'active' );
+			}
+		);
+
+		// End basic front-end scripts here
+	}
+);
