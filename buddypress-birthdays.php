9e1c557193aff3d03a8f2a79ec766b71c6b5ab50
Varun Kumar Dubey <vapvarun@gmail.com>
2022-09-15 17:39:18 +0530

    Update buddypress-birthdays.php

--- buddypress-birthdays.php
+++ buddypress-birthdays.php
@@ -3,7 +3,7 @@
  * Plugin Name: Wbcom Designs - Birthday Widget for BuddyPress
  * Plugin URI: https://wbcomdesigns.com/downloads/buddypress-birthdays/
  * Description: Display upcoming birthdays
- * Version: 1.8.0
+ * Version: 1.8.1
  * Author: Wbcom Designs
  * Author URI: https://wbcomdesigns.com/
  * Text Domain: buddypress-birthdays
