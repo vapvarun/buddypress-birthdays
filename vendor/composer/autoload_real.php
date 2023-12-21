12068a61c607a37ab7cdb1a5fcddd60ec9f7af49
vapvarun <vapvarun@gmail.com>
2023-12-21 11:15:47 +0530

    Added BP 12 support

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,11 +23,30 @@ class ComposerAutoloaderInitaf5555ee1362075472616689742ca7d1
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitaf5555ee1362075472616689742ca7d1', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitaf5555ee1362075472616689742ca7d1', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInitaf5555ee1362075472616689742ca7d1::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInitaf5555ee1362075472616689742ca7d1::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(true);
 
